6f5cb06ccad2f60975cdfc9549952b41fb4a6df3
jcrauhut <jrauhut@hotmail.ca>
2020-04-24 16:14:38 -0400

    remove api key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai')
 
 const app = new Clarifai.App({
-    apiKey: '51401f4917994a31916909d670079629'
+    apiKey: process.env.API_CLARIFAI
    });
 
 const handleApiCall = (req, res) => {
